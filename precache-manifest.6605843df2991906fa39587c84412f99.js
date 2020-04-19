d32f4bc372430e99c0e4efcd395333aadab9a774
Ihor Radchuk <radchuk2808@gmail.com>
2020-04-19 21:22:06 +0300

    Updates

---
+++ precache-manifest.6605843df2991906fa39587c84412f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9bc03f88319dff5e1b480e77a892461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99017e44d966438ef5e0",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "47de10d8e8145fc06ceb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99017e44d966438ef5e0",
+    "url": "/static/js/2.7e504480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e504480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47de10d8e8145fc06ceb",
+    "url": "/static/js/main.1c72b199.chunk.js"
+  },
+  {
+    "revision": "ac4390a29cfab16e82a4",
+    "url": "/static/js/runtime-main.ba23e9d3.js"
+  }
+]);
